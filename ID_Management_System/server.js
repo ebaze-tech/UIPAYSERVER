2427c80550f1f02f4c544093158a93bbb948cbe7
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-10-20 20:47:29 +0100

    Update server.js

--- ID_Management_System/server.js
+++ ID_Management_System/server.js
@@ -3,7 +3,45 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 // const app = express
-require("./DatabaseServer/db");
+//require("./DatabaseServer/db");
+
+const { Sequelize } = require("sequelize");
+const http = require("http");
+const server = http.createServer(app);
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8mb4",
+    },
+    logging: false,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected...");
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error:" + error);
+    process.exit(1);
+  });
+
+
+
+
+
+
+
 
 app.use(
   cors({
