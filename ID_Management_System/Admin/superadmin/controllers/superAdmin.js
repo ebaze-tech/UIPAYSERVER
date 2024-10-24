6617bd802b0959e753ac23c45d79f55bfe5cdc0e
ebaze-tech <ebaze.technologies@gmail.com>
2024-10-24 02:47:10 +0100

    Added to the admin server

--- ID_Management_System/Admin/superadmin/controllers/superAdmin.js
+++ ID_Management_System/Admin/superadmin/controllers/superAdmin.js
@@ -7,7 +7,20 @@ exports.getAllRequests = async (req, res) => {
     const requests = await requestModel.findAll({
       where: { status: "Pending" },
     });
-    if (!requests) {
+    if (!requests || requests.length === 0) {
+      return res.status(404).json({ message: "Request not found." });
+    }
+    res.status(200).json(requests);
+  } catch (error) {
+    res.status(500).json({ message: "Server error.", error: error.message });
+  }
+};
+exports.viewAllApprovedRequests = async (req, res) => {
+  try {
+    const requests = await requestModel.findAll({
+      where: { status: "Approved" },
+    });
+    if (!requests || requests.length === 0) {
       return res.status(404).json({ message: "Request not found." });
     }
     res.status(200).json(requests);
@@ -16,12 +29,75 @@ exports.getAllRequests = async (req, res) => {
   }
 };
 
+exports.getAllRequestsByUserType = async (req, res) => {
+  const { userType } = req.params;
+  try {
+    const requests = await requestModel.findAll({
+      where: {
+        status: "Pending",
+        userType,
+      },
+    });
+    if (!requests || requests.length === 0) {
+      return res.status(404).json({ message: "Request not found." });
+    }
+    res.status(200).json(requests);
+  } catch (error) {
+    res.status(500).json({ message: "Server error.", error: error.message });
+  }
+};
+
+exports.getRequestByIdAndUserType = async (req, res) => {
+  const { id, userType } = req.params;
+  try {
+    const request = await requestModel.findOne({
+      where: {
+        id,
+        userType,
+      },
+    });
+    if (!request || request.length === 0) {
+      return res.status(404).json({ message: "Request not found." });
+    }
+    res.status(200).json(request);
+  } catch (error) {
+    res.status(500).json({ message: "Server error:", error: error.message });
+  }
+};
+
+exports.updateRequestStatusByUserType = async (req, res) => {
+  const { id } = req.params;
+  const { status, userType } = req.body; // get userType from body
+  try {
+    const request = await requestModel.findOne({
+      where: {
+        id,
+        userType, // Filter by userType
+      },
+    });
+    if (request) {
+      request.status = status;
+      await request.save();
+      res
+        .status(201)
+        .json({ message: "Request updated successfully", request });
+    } else {
+      return res.status(404).json({
+        message:
+          "Request not found or doesn't belong to the specified user type.",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error updating request", error });
+  }
+};
+
 // Get a specific request by ID
 exports.getRequestsById = async (req, res) => {
   const { id } = req.params;
   try {
     const request = await requestModel.findByPk(id);
-    if (!request) {
+    if (!request || request.length === 0) {
       return res.status(404).json({ message: "Request not found." });
     }
     res.status(200).json(request); // Return the found request
@@ -36,7 +112,7 @@ exports.updateRequestStatus = async (req, res) => {
   const { status } = req.body;
   try {
     const request = await requestModel.findByPk(id);
-    if (!request) {
+    if (!request || request.length === 0) {
       return res.status(404).json({ message: "Request not found." });
     }
 
--- ID_Management_System/Admin/superadmin/routes/superAdmin.js
+++ ID_Management_System/Admin/superadmin/routes/superAdmin.js
@@ -3,22 +3,58 @@ const requestController = require("../controllers/superAdmin");
 const { authenticate, authorize } = require("../../middlewares/Auth");
 const router = express.Router();
 
-router.get(
-  "/requests",
-  authenticate,
-  authorize("SuperAdmin"), 
-  requestController.getAllRequests
-);
-router.get(
-  "/requests/:id",
-  authenticate,
-  authorize("SuperAdmin"), 
-  requestController.getRequestsById
-);
+// Route to approve or reject a request
 router.put(
   "/requests/:id",
   authenticate,
-  authorize("SuperAdmin"), 
+  authorize("SuperAdmin"),
   requestController.updateRequestStatus
+);
+
+// Route to view all requests
+router.get(
+  "/requests",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.getAllRequests
+);
+// Route to view by id of userType
+router.get(
+  "/requests/:id",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.getRequestsById
+);
+
+// Route to view by userType(staff or student)
+router.get(
+  "/requests/:userType",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.getAllRequestsByUserType
+);
+
+// Route to view requests by userType(staff or student) and id of userType
+router.get(
+  "/requests/:userType/:id",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.getRequestByIdAndUserType
+);
+
+// Route to approve or reject the request by userType(staff or student) and id of userType
+router.put(
+  "/requests/:userType/:id",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.getRequestByIdAndUserType
+);
+
+// Route to view all approved requests
+router.get(
+  "/requests/approved",
+  authenticate,
+  authorize("SuperAdmin"),
+  requestController.viewAllApprovedRequests
 );
 module.exports = router;
